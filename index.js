ef22d8a98d8b52f35e7a3f89a2f3b36f064ff3e4
MarceIsid <marc.segovia@duocuc.cl>
2025-04-23 10:21:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { VoiceResponse } = require('twilio').twiml;
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Ruta inicial de la llamada
+app.post('/voice', (req, res) => {
+  const twiml = new VoiceResponse();
+
+  const gather = twiml.gather({
+    numDigits: 1,
+    action: '/question1',
+    method: 'POST'
+  });
+
+  gather.say('Hola. Gracias por responder esta llamada. Vamos a hacerte una breve encuesta.');
+  gather.say('Primera pregunta. ¿Te interesa empezar un curso este mes? Presiona 1 para sí, 2 para no.');
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+// Procesa respuesta de pregunta 1
+app.post('/question1', (req, res) => {
+  const digit = req.body.Digits;
+  const twiml = new VoiceResponse();
+
+  if (digit === '1') {
+    const gather = twiml.gather({
+      numDigits: 1,
+      action: '/question2',
+      method: 'POST'
+    });
+
+    gather.say('Excelente. Segunda pregunta. ¿Tienes el presupuesto para comenzar? Presiona 1 para sí, 2 para no.');
+  } else {
+    twiml.say('Gracias por tu tiempo. Hasta luego.');
+    twiml.hangup();
+  }
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+// Procesa respuesta de pregunta 2
+app.post('/question2', (req, res) => {
+  const digit = req.body.Digits;
+  const twiml = new VoiceResponse();
+
+  // Aquí podrías guardar las respuestas en una base de datos si lo deseas
+  twiml.say('Gracias por tus respuestas. Un asesor se pondrá en contacto contigo pronto.');
+  twiml.hangup();
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+app.listen(port, () => {
+  console.log(`Servidor de encuesta corriendo en http://localhost:${port}`);
+});
