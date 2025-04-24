b9a417982ac88724e02693a62f7b090b7a81aa25
MarceIsid <marc.segovia@duocuc.cl>
2025-04-24 11:12:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/voice', (req, res) => {
     language: 'es-MX',
     action: '/question1',
     method: 'POST',
-    timeout: 5
+    timeout: 2
   });
 
   gather.say({ language: 'es-MX', voice: 'Polly.Conchita' }, 'Primera pregunta. ¿Te interesa empezar un curso este mes? Puedes responder sí o no.');
@@ -42,7 +42,7 @@ app.post('/question1', (req, res) => {
       language: 'es-MX',
       action: '/question2',
       method: 'POST',
-      timeout: 5
+      timeout: 2
     });
 
     gather.say({ language: 'es-MX', voice: 'Polly.Conchita' }, 'Segunda pregunta. ¿Tienes el presupuesto para comenzar? Puedes responder sí o no.');
